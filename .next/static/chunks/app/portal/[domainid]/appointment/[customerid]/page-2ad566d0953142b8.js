3bdd73caa5cec84b658a8481f426f7ead83a175a
LuluWorm3 <132191883+LuluWorm3@users.noreply.github.com>
2024-10-03 12:53:23 +0300

    Update .gitignore to exclude unnecessary files

--- .next/static/chunks/app/portal/[domainid]/appointment/[customerid]/page-2ad566d0953142b8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3749],{},function(n){n.O(0,[8218,172,5555,8173,654,6582,2492,1310,4002,2971,7023,1744],function(){return n(n.s=4002)}),_N_E=n.O()}]);
